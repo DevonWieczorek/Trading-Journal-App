a99292f32c0046d1c4d8670f653eb618fb9d7f76
Devon Wieczorek <devon.wieczorek93@gmail.com>
2021-02-15 15:33:46 -0500

    Built app from tutorial
    https://www.freecodecamp.org/news/how-to-build-a-todo-application-using-reactjs-and-firebase/

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDKjj868o0FV82JqkjjElCAB9-7Q-lZFbQ",
+    authDomain: "trading-journal-4381e.firebaseapp.com",
+    databaseURL: "https://trading-journal-4381e-default-rtdb.firebaseio.com",
+    projectId: "trading-journal-4381e",
+    storageBucket: "trading-journal-4381e.appspot.com",
+    messagingSenderId: "806253761524",
+    appId: "1:806253761524:web:d94457013e1a7f47019fa1"
+};
